5c4696c45e627f9d5b1f0ca1c375e2de55eea1cc
Sophia <gmb728@gmail.com>
2020-02-18 09:13:42 +0800

    Initial Commit

---
+++ SumikkoPairing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SumikkoPairing
+//
+//  Created by Chang Sophia on 2/18/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
